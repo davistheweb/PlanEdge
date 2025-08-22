fa07dd7bb80bfedf90e27c38d9e0c96baf692677
davistheweb <josiahdave001@gmail.com>
2025-08-22 11:41:36 +0100

    proxy fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable 
+class User extends Authenticatable implements MustVerifyEmail
 {
-// implements MustVerifyEmail
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
